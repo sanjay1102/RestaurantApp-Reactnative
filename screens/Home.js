fd5c4a57c9a31f3f3d88b133608272c535e95bc6
sanjay1102 <sanjujay112@gmail.com>
2022-05-27 12:00:54 +0530

    init

---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { SafeAreaView, View } from 'react-native'
+import Categories from '../components/Categories'
+import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
+
+
+export default function Home() {
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee",}}>
+        <View style={{backgroundColor: "white", padding: 15}}>
+            <HeaderTabs/>
+            <SearchBar/>
+            <Categories/>
+        </View>
+    </SafeAreaView>
+  )
+}
